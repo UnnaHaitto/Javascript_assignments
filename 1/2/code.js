744cee43eaaebed65a33662db98d6315b5b23800
UnnaHaitto <125347964+UnnaHaitto@users.noreply.github.com>
2026-03-26 13:36:21 +0200

    Javascript 1 assignments

---
+++ 1/2/code.js
@@ -0,0 +1,3 @@
+let name = prompt('Anna nimi: ');
+
+document.querySelector('#prompt').innerHTML = 'Good morning, ' + name + '!';
---
+++ 1/3/code.js
@@ -0,0 +1,11 @@
+let number1 = +prompt("Anna numero 1");
+let number2 = +prompt("Anna numero 2");
+let number3 = +prompt("Anna numero 3");
+
+let sum = number1 + number2 + number3;
+let product = number1 * number2 * number3;
+let average = sum / 3;
+
+document.querySelector("#show").innerHTML = "Summa " + sum + " tulo " + product + " keskiarvo " + average;
+
+
