36fd2dbf64148c81c75eb5b696d2b14b2c00720f
UnnaHaitto <125347964+UnnaHaitto@users.noreply.github.com>
2026-03-30 14:57:16 +0300

    Javascript 2 assignments

---
+++ 2/1/code.js
@@ -0,0 +1,13 @@
+const numbers = []
+
+let n = 0
+
+while (n < 5){
+    let number = +prompt("Anna numero")
+    numbers.push(number)
+    n++
+}
+
+for (let i = 4; i >= 0; i--){
+    document.querySelector("#print").innerHTML += numbers[i]
+}
