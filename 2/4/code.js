67a98e45b64c75db910c684c72f4fde0551ca233
UnnaHaitto <125347964+UnnaHaitto@users.noreply.github.com>
2026-03-30 16:23:23 +0300

    Javascript 2 assignments

--- 2/4/code.js
+++ 2/4/code.js
@@ -1,1 +1,14 @@
-console.log("Im printing to console!")
+const number_list = [];
+
+let number;
+do{
+    number = +prompt("Give a number. 0 stops");
+    if(number !== 0) {
+        number_list.push(number);
+    }
+} while (number !== 0);
+
+number_list.sort();
+number_list.reverse();
+
+console.log(number_list);
