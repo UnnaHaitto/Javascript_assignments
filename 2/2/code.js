44814036b9d98441d849bdc61144430a2489889b
UnnaHaitto <125347964+UnnaHaitto@users.noreply.github.com>
2026-03-30 15:30:01 +0300

    Javascript 2 assignments

--- 2/2/code.js
+++ 2/2/code.js
@@ -1,1 +1,16 @@
-console.log("Im printing to console!")
+//document.querySelector("#list").innerHTML = ""
+
+let amount = +prompt("Give the number of participants");
+
+const participant_list = [];
+
+for (let i = amount; i > 0; i--){
+    let name = prompt("Give the name of the participant");
+    participant_list.push(name);
+}
+
+console.log(participant_list);
+
+for (let name of participant_list){
+    document.querySelector("#list").innerHTML += "<li>" + name + "</li>";
+}
